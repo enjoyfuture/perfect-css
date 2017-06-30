0573e38ac53e64a27d985e99fa02d0519d80b7f6
wangyanjun <linder0209@126.com>
2017-06-30 16:48:52 +0800

    Update gulp config

--- gulp/scss.babel.js
+++ gulp/scss.babel.js
@@ -16,9 +16,14 @@ const scssOption = {
  * 浏览器支持性参考 bootstrap 官方的支持，参见源码 postcss.js 的支持性说明
  * iOS：https://developer.apple.com/support/app-store/
  * Android：https://developer.android.com/about/dashboards/index.html
+ * http://tongji.baidu.com/data/browser
  */
-const browsers = ['Chrome >= 35', 'Firefox >= 38', 'Edge >= 12',
-  'Explorer >= 10', 'Android >= 4', 'iOS >= 8', 'Safari >= 8', 'Opera >= 12'];
+// 对于低版本浏览器的兼容性待处理
+/*const browsers = ['Chrome >= 35', 'Firefox >= 38', 'Edge >= 12',
+  'Explorer >= 10', 'Android >= 4', 'iOS >= 8', 'Safari >= 8', 'Opera >= 12'];*/
+
+const browsers = ['Chrome >= 43', 'Firefox >= 38', 'Edge >= 12',
+  'Explorer >= 10', 'Android >= 4.4', 'iOS >= 9', 'Safari >= 9', 'Opera >= 30'];
 
 const postcssPlugins = [
   autoprefixer({browsers}),
@@ -42,7 +47,8 @@ gulp.task('scss', ['scss-lint'], () => {
 
 // 编译 docs scss
 gulp.task('scss-docs', ['scss-lint'], () => {
-  return gulp.src(['scss/perfect.scss', 'docs/assets/scss/docs.scss'])
+  return gulp.src(['scss/perfect.scss', 'docs/assets/scss/docs.scss',
+    'docs/assets/scss/device-view.scss'])
     .pipe($.sourcemaps.init())
     .pipe($.sass.sync(scssOption).on('error', $.sass.logError))
     .pipe($.postcss(postcssPlugins))
