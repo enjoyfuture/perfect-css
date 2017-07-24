205340a23edec96ae0b0fa36e401b5d457df7434
wangyanjun <linder0209@126.com>
2017-07-24 21:36:55 +0800

    scss 单元测试

--- test/scss/scss-test.js
+++ test/scss/scss-test.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const sassTrue = require('sass-true');
+const functions = require('../../scss/js-function/Math');
 
 // 待测试的文件
-const officialDemo = path.join(__dirname, 'official-demo', 'test.scss');
-
+// const officialTest = path.join(__dirname, 'official-demo', 'test.scss');
 // 运行测试文件
-sassTrue.runSass({file: officialDemo}, describe, it);
+// sassTrue.runSass({file: officialTest}, describe, it);
+
+// 待测试的文件
+const perfectTest = path.join(__dirname, 'perfect', 'perfect-test.scss');
+sassTrue.runSass({
+  file: perfectTest,
+  functions,
+  precision: 15
+}, describe, it);
