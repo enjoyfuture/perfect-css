e982a1382df80ee37acfc5ee927b991549925506
wangyanjun <linder0209@126.com>
2017-12-04 23:28:52 +0800

    调整 image 例子

--- docs/components/elements/generate-image.js
+++ docs/components/elements/generate-image.js
@@ -1,4 +1,15 @@
 (function () {
+  // 计算颜色对比色，支持 RGB 格式
+  function contrast(color) {
+    const rgb = [parseInt(color.substring(1, 3), 16),
+      parseInt(color.substring(3, 5), 16),
+      parseInt(color.substring(5, 7), 16)];
+    for (let i = 0; i < 3; i++) {
+      rgb[i] = 255 - rgb[i];
+    }
+    return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+  }
+
   /**
    * 生成图片
    generateImage({
@@ -22,8 +33,8 @@
       height = size[1],
       fontFamily = 'sans-serif';
 
-    if (!fontSize) { // 取宽度的 1/15
-      fontSize = width / 15;
+    if (!fontSize) { // 取宽度的 1/8
+      fontSize = (width / 8).toFixed();
     }
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
@@ -33,11 +44,22 @@
     ctx.fillStyle = background;
     ctx.fillRect(0, 0, width, height);
 
+    // 背景上设置小点
+    ctx.fillStyle = contrast(background);
+    const pointX = width / 2;
+    const pointY = height / 2;
+    for (let i = 0; i < pointY; i++) {
+      for (let j = 0; j < pointX; j++) {
+        ctx.fillRect(i * 20 + 9, j * 20 + 9, 2, 2);
+      }
+    }
+
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     ctx.fillStyle = color;
     ctx.font = `bold ${fontSize}px ${fontFamily}`;
     ctx.fillText(text, (width / 2), (height / 2), width);
+
     return canvas.toDataURL('image/png');
   }
 
